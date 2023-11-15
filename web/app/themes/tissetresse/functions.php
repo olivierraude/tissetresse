13c003f3032100770ed9a52b59f6320ea4611983
olivierraude <olivierraude@gmail.com>
2023-11-14 22:32:28 -0500

    first base

---
+++ web/app/themes/tissetresse/functions.php
@@ -0,0 +1,23 @@
+<?php
+
+function theme_supports()
+{
+  add_theme_support('title-tag');
+}
+
+function theme_register_assets()
+{
+  wp_register_style('monstyle', get_stylesheet_directory_uri() . '/sass/style.css');
+
+  wp_register_script('gsap', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js', [], false, true);
+  wp_register_script('monscript', get_stylesheet_directory_uri() . '/js/main.js', [], false, true);
+
+  wp_enqueue_style('monstyle');
+
+  wp_enqueue_script('gsap');
+  wp_enqueue_script('monscript');
+}
+
+
+add_action('after_setup_theme', 'theme_supports');
+add_action('wp_enqueue_scripts', 'theme_register_assets');
---
+++ web/app/themes/tissetresse/index.php
@@ -0,0 +1,5 @@
+<?php get_header() ?>
+
+<h1>Welcome to your new Wordpress Theme</h1>
+
+<?php get_footer() ?>
